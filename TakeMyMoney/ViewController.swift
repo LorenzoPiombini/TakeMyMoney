ea40e22114e57dbc67084275bae4a5c735be295d
LorenzoPiombini <69109614+LorenzoPiombini@users.noreply.github.com>
2020-11-17 20:34:53 -0500

    Starting point

---
+++ TakeMyMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeMyMoney
+//
+//  Created by Lorenzo piombini on 11/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
