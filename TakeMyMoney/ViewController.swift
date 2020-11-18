9dc3233164a652b94e3e5f2223b04c3ad9e5725e
LorenzoPiombini <69109614+LorenzoPiombini@users.noreply.github.com>
2020-11-17 23:26:36 -0500

    animation added

--- TakeMyMoney/ViewController.swift
+++ TakeMyMoney/ViewController.swift
@@ -7,16 +7,38 @@
 
 import UIKit
 
+@IBDesignable
 class ViewController: UIViewController {
 
-    @IBOutlet weak var scrollViewtohide: UIScrollView!
+    override class func prepareForInterfaceBuilder() {
+        self
+    }
     
+    @IBOutlet weak var scrollViewtohide: UIScrollView!
+    @IBOutlet weak var textFieldWithInTheScrollView: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        scrollViewtohide.isHidden = true
+        //to hide the scrollview :
+        scrollViewtohide.alpha = 0
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showingPayPal(_ sender: UIButton){
+        
+        UIView.animate(withDuration: 0.30, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: { self.scrollViewtohide.alpha = 0.0}, completion: nil)
+       
+        
+    }
+    
+    @IBAction func showingTheScrollView(_ sender: UIButton){
+        
+        UIView.animate(withDuration: 0.30, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {self.scrollViewtohide.isHidden = false; self.scrollViewtohide.alpha = 1.0}, completion: nil)
+        
+        
+        
+    
+        
+    }
 
 }
 
